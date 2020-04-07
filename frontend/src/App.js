f54bc8de514e053cb2a82e13e9f5b0d8f7c49363
Deivid Nascimento <deivid.magno.nascimento@gmail.com>
2020-04-06 22:03:32 -0300

    preparação do ambiente para construção do frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Routes from './routes';
+import Globalstyles from './styles/global';
+
+function App() {
+  return (
+    <>
+      <Routes />
+      <Globalstyles />
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/Main/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Title } from './styles';
+
+// import { Container } from './styles';
+
+export default function Main() {
+  return <Title>Hello World!!!</Title>;
+}
